253620c4bc56ae64e6404bb293e2419bde694c9b
mohamed maghraby <mohamed.maghraby@artlinkgroup.com>
2024-02-11 15:49:27 +0200

    git init

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text} from 'react-native'
+import { useFonts } from 'expo-font';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './screens/Home';
+import Welcome from './screens/Welcome';
+import NFTDetails from './screens/NFTDetails';
 
 export default function App() {
+
+  const [fontLoaded] = useFonts({
+    InterBold: require('./assets/fonts/Inter-Bold.ttf'),
+    InterLight: require('./assets/fonts/Inter-Light.ttf'),
+    InterMedium: require('./assets/fonts/Inter-Medium.ttf'),
+    InterRegular: require('./assets/fonts/Inter-Regular.ttf'),
+    InterSemiBold: require('./assets/fonts/Inter-SemiBold.ttf')
+  })
+
+  if(!fontLoaded) return null;
+
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="light" animated={true}/>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName='Welcome'
+          screenOptions={{ 
+            headerShown: false
+           }}
+        >
+          <Stack.Screen name="welcome" component={Welcome} />
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="NFT-details" component={NFTDetails} />
+        </Stack.Navigator>
+      </NavigationContainer>
+
+    </>
   );
 }
 
